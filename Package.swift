1241e42edf3d6f77712f50e6bc8defdfdc0ed04a
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-08 08:19:57 +0100

    Remove inappropriate Apple platform constraints

--- Package.swift
+++ Package.swift
@@ -4,12 +4,6 @@ import PackageDescription
 
 let package = Package(
     name: "swift-windows",
-    platforms: [
-        .macOS(.v26),
-        .iOS(.v26),
-        .tvOS(.v26),
-        .watchOS(.v26)
-    ],
     products: [
         .library(
             name: "Windows Primitives",
